2df42f31f23e536e80fb2b8da54263f51e6d0455
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2023-10-26 19:38:32 -0500

    finish removing pet transfer event

--- src/main/java/net/thedudemc/dudeutils/init/PluginEvents.java
+++ src/main/java/net/thedudemc/dudeutils/init/PluginEvents.java
@@ -2,7 +2,6 @@ package net.thedudemc.dudeutils.init;
 
 import net.thedudemc.dudeutils.DudeUtils;
 import net.thedudemc.dudeutils.event.MiscEvents;
-import net.thedudemc.dudeutils.event.PetTransferEvent;
 import net.thedudemc.dudeutils.event.PlayerEvents;
 import net.thedudemc.dudeutils.util.Log;
 import org.bukkit.plugin.Plugin;
@@ -15,6 +14,5 @@ public class PluginEvents {
 
         plugin.getServer().getPluginManager().registerEvents(new MiscEvents(), plugin);
         plugin.getServer().getPluginManager().registerEvents(new PlayerEvents(), plugin);
-        plugin.getServer().getPluginManager().registerEvents(new PetTransferEvent(), plugin);
     }
 }
